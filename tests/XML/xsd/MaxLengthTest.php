374798a9167e663c7f63872c352b6029623163d7
Tim van Dijen <tvdijen@gmail.com>
2025-06-12 20:33:07 +0200

    Add xsd:maxLength

---
+++ tests/XML/xsd/MaxLengthTest.php
@@ -0,0 +1,131 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SimpleSAML\XSD\Test\XML\xsd;
+
+use DOMText;
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\Attributes\Group;
+use PHPUnit\Framework\TestCase;
+use SimpleSAML\XML\Attribute as XMLAttribute;
+use SimpleSAML\XML\Constants as C;
+use SimpleSAML\XML\DOMDocumentFactory;
+use SimpleSAML\XML\TestUtils\{SchemaValidationTestTrait, SerializableElementTestTrait};
+use SimpleSAML\XML\Type\{AnyURIValue, BooleanValue, IDValue, NonNegativeIntegerValue, StringValue};
+use SimpleSAML\XSD\XML\xsd\AbstractAnnotated;
+use SimpleSAML\XSD\XML\xsd\AbstractFacet;
+use SimpleSAML\XSD\XML\xsd\AbstractOpenAttrs;
+use SimpleSAML\XSD\XML\xsd\AbstractXsdElement;
+use SimpleSAML\XSD\XML\xsd\Annotation;
+use SimpleSAML\XSD\XML\xsd\Appinfo;
+use SimpleSAML\XSD\XML\xsd\Documentation;
+use SimpleSAML\XSD\XML\xsd\MaxLength;
+
+use function dirname;
+use function strval;
+
+/**
+ * Tests for xsd:MaxLength
+ *
+ * @package simplesamlphp/xml-xsd
+ */
+#[Group('xsd')]
+#[CoversClass(MaxLength::class)]
+#[CoversClass(AbstractFacet::class)]
+#[CoversClass(AbstractAnnotated::class)]
+#[CoversClass(AbstractOpenAttrs::class)]
+#[CoversClass(AbstractXsdElement::class)]
+final class MaxLengthTest extends TestCase
+{
+    use SchemaValidationTestTrait;
+    use SerializableElementTestTrait;
+
+
+    /**
+     */
+    public static function setUpBeforeClass(): void
+    {
+        self::$testedClass = MaxLength::class;
+
+        self::$xmlRepresentation = DOMDocumentFactory::fromFile(
+            dirname(__FILE__, 3) . '/resources/xml/maxLength.xml',
+        );
+    }
+
+
+    // test marshalling
+
+
+    /**
+     * Test creating an MaxLength object from scratch.
+     */
+    public function testMarshalling(): void
+    {
+        $appinfoDocument = DOMDocumentFactory::create();
+        $text = new DOMText('Application Information');
+        $appinfoDocument->appendChild($text);
+
+        $otherAppinfoDocument = DOMDocumentFactory::create();
+        $otherText = new DOMText('Other Application Information');
+        $otherAppinfoDocument->appendChild($otherText);
+
+        $documentationDocument = DOMDocumentFactory::create();
+        $text = new DOMText('Some Documentation');
+        $documentationDocument->appendChild($text);
+
+        $otherDocumentationDocument = DOMDocumentFactory::create();
+        $text = new DOMText('Other Documentation');
+        $otherDocumentationDocument->appendChild($text);
+
+        $attr1 = new XMLAttribute('urn:x-simplesamlphp:namespace', 'ssp', 'attr1', StringValue::fromString('value1'));
+        $attr2 = new XMLAttribute('urn:x-simplesamlphp:namespace', 'ssp', 'attr2', StringValue::fromString('value2'));
+        $attr3 = new XMLAttribute('urn:x-simplesamlphp:namespace', 'ssp', 'attr3', StringValue::fromString('value3'));
+        $attr4 = new XMLAttribute('urn:x-simplesamlphp:namespace', 'ssp', 'attr4', StringValue::fromString('value4'));
+        $langattr = new XMLAttribute(C::NS_XML, 'xml', 'lang', StringValue::fromString('nl'));
+
+        $appinfo1 = new Appinfo(
+            $appinfoDocument->childNodes,
+            AnyURIValue::fromString('urn:x-simplesamlphp:source'),
+            [$attr1],
+        );
+        $appinfo2 = new Appinfo(
+            $otherAppinfoDocument->childNodes,
+            AnyURIValue::fromString('urn:x-simplesamlphp:source'),
+            [$attr2],
+        );
+
+        $documentation1 = new Documentation(
+            $documentationDocument->childNodes,
+            $langattr,
+            AnyURIValue::fromString('urn:x-simplesamlphp:source'),
+            [$attr1],
+        );
+        $documentation2 = new Documentation(
+            $otherDocumentationDocument->childNodes,
+            $langattr,
+            AnyURIValue::fromString('urn:x-simplesamlphp:source'),
+            [$attr2],
+        );
+
+        $annotation = new Annotation(
+            [$appinfo1, $appinfo2],
+            [$documentation1, $documentation2],
+            IDValue::fromString('phpunit_annotation'),
+            [$attr3],
+        );
+
+        $MaxLength = new MaxLength(
+            NonNegativeIntegerValue::fromInteger(1024),
+            BooleanValue::fromBoolean(true),
+            $annotation,
+            IDValue::fromString('phpunit_maxlength'),
+            [$attr4],
+        );
+
+        $this->assertEquals(
+            self::$xmlRepresentation->saveXML(self::$xmlRepresentation->documentElement),
+            strval($MaxLength),
+        );
+    }
+}
