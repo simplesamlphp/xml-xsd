486e4b15f39dd6760a28cf87784d7f624aeb3422
Tim van Dijen <tvdijen@gmail.com>
2025-06-14 23:54:38 +0200

    Add xs:simpleDerivationSet type

---
+++ src/Type/SimpleDerivationSetValue.php
@@ -0,0 +1,53 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SimpleSAML\XSD\Type;
+
+use SimpleSAML\XML\Assert\Assert;
+use SimpleSAML\XML\Constants as C;
+use SimpleSAML\XML\Exception\SchemaViolationException;
+use SimpleSAML\XML\Type\NMTokenValue;
+use SimpleSAML\XML\XsNamespace;
+use SimpleSAML\XSD\XML\xsd\DerivationControlEnum;
+// use SimpleSAML\XSD\XML\xsd\NameSpaceEnum;
+
+use function explode;
+
+/**
+ * @package simplesaml/xml-xsd
+ */
+class SimpleDerivationSetValue extends NMTokenValue
+{
+    /** @var string */
+    public const SCHEMA_TYPE = 'simpleDerivationSet';
+
+
+    /**
+     * Validate the value.
+     *
+     * @param string $value  The value
+     * @throws \Exception on failure
+     * @return void
+     */
+    protected function validateValue(string $value): void
+    {
+        $sanitized = $this->sanitizeValue($value);
+
+        if ($sanitized !== '#all' && $sanitized !== '') {
+//        if ($sanitized !== NamespaceEnum::Any) {
+            $list = explode(' ', $sanitized, C::UNBOUNDED_LIMIT);
+
+            // After filtering the allowed values, there should be nothing left
+            $filtered = array_diff(
+                $list,
+                [
+                    DerivationControlEnum::List->value,
+                    DerivationControlEnum::Restriction->value,
+                    DerivationControlEnum::Union->value,
+                ],
+            );
+            Assert::isEmpty($filtered, SchemaViolationException::class);
+        }
+    }
+}
